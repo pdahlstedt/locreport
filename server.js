c5e7505ce8a5dd028bcddd28b5a5a0e369407249
petter.d <petter.dahlstedt@outlook.com>
2018-07-08 08:57:14 +0200

    added server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var http = require('http');
+var fs = require('fs');
 
 function onRequest(request, response) {
-    response.writeHead(200, {'Content-type': 'text/plain'});
-    response.write('Hello world');
-    response.end();
-};
+    response.writeHead(200, {'Content-type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data){
+        if(error){
+            response.writeHead(404);
+            response.write('File not found')
+        } else {
+            response.write(data);
+        }
+        response.end();
+    })
+   };
 
 http.createServer(onRequest).listen(8000);
 
